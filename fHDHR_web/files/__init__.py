ae3a07b5736dc616ab3f16358e2e1325d2aa84ea
DanAustinGH <Dan_Austin@fitawi.com>
2020-12-20 20:59:11 -0700

    Changes tables to div and added CSS elements

--- fHDHR_web/files/__init__.py
+++ fHDHR_web/files/__init__.py
@@ -3,7 +3,8 @@
 from .favicon_ico import Favicon_ICO
 from .style_css import Style_CSS
 from .device_xml import Device_XML
-
+from .brython import Brython
+from .brython_stdlib import Brython_stdlib
 
 class fHDHR_Files():
 
@@ -13,3 +14,5 @@ class fHDHR_Files():
         self.favicon = Favicon_ICO(fhdhr)
         self.style = Style_CSS(fhdhr)
         self.device_xml = Device_XML(fhdhr)
+        self.brython = Brython(fhdhr)
+        self.brython_stdlib = Brython_stdlib(fhdhr)
